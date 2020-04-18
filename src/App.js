f258f083e756386e1a4d7693420cc2cc8fa01466
nachiketkallapur <nachiketkallapur@gmail.com>
2020-04-18 14:39:50 +0530

    Just added two more components in app.js for understanding React Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import HomePage from './pages/homepage.component'
 import './App.css';
 
+const TopicsList = ()=>{
+  return <h1>TOPICS LIST</h1>
+}
+const TopicsDetail = () =>{
+  return <h1>TOPICS DETAIL</h1>
+}
+
 function App() {
   return (
     <div>
