e61cdca33652a7d2b30136b027359f0f0e7fa89a
nachiketkallapur <nachiketkallapur@gmail.com>
2020-04-18 15:28:12 +0530

    Added some test components to understand match and locaion props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import HomePage from './pages/homepage.component'
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+};
+const TopicsList = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>TOPICS LIST</h1>
+      <Link to={`${props.match.url}/nachiket`}>To TopicId 12</Link>
+    </div>
+  )
+};
+const TopicsDetail = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>TOPICS DETAIL{props.match.params.topicId}</h1>
+    </div>
+  )
+};
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route exact={true} path="/topics" component={TopicsList} />
+        <Route path="/topics/:topicId" component={TopicsDetail} />
       </Switch>
     </div>
   );
