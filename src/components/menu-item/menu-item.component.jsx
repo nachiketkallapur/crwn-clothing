70e37ceaac83f0733a09f699fd894da03aa96383
nachiketkallapur <nachiketkallapur@gmail.com>
2020-04-19 10:20:56 +0530

    Notihng

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-import './menu-item.styles.scss'
+import './menu-item.styles.scss';
 
-function MenuItem({title,imageUrl,size}) {
-    return (
-        <div 
-        className={`${size} menu-item`}>
-            <div className="background-image" style={{
-            backgroundImage:`url(${imageUrl})`
-        }} / >
-            <div className="content">
-                <h1 className="title">{title.toUpperCase()}</h1>
-                <span className="subtitle">SHOP NOW</span>
-            </div>
-        </div>
-    )
+function MenuItem({title,imageUrl,size}){
+  return(
+    <div
+    className={`${size} menu-item`}
+  >
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+  )
 }
 
-export default MenuItem
+export default withRouter(MenuItem);
