9f35efd0fd0613183d9fbd16fdb9ae8ae9bbe1bd
nachiketkallapur <nachiketkallapur@gmail.com>
2020-04-21 23:50:26 +0530

    created cutom form component and custom-button component

---
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './sign-in-and-sign-up.styles.scss'
+import SignIn from '../sign-in/sign-in.component.jsx'
+
+function SignInAndSignUpPage() {
+    return (
+        <div className="sign-in-and-sign-up">
+            <SignIn/>
+        </div>        
+        
+    )
+}
+
+export default SignInAndSignUpPage
