115c45cc4a0b32cd37ae5b0317feda3f2e46fa6d
nachiketkallapur <nachiketkallapur@gmail.com>
2020-05-26 12:19:49 +0530

    Minor change

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,19 +1,19 @@
-const INITIAL_STATE = {
-    currentUser: null
-};
+// const INITIAL_STATE = {
+//     currentUser: null
+// };
 
-// state = INITIAL_STATE  means that if state is not defined(not set) it takes the default value
+// // state = INITIAL_STATE  means that if state is not defined(not set) it takes the default value
 
-const userReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case 'SET_CURRENT_USER':
-            return {
-                ...state,
-                currentUser: action.payload
-            }
+// const userReducer = (state = INITIAL_STATE, action) => {
+//     switch (action.type) {
+//         case 'SET_CURRENT_USER':
+//             return {
+//                 ...state,
+//                 currentUser: action.payload
+//             }
 
-        default: state
-    }
-}
+//         default: state
+//     }
+// }
 
-export default userReducer;
+// export default userReducer;
